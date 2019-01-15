86d173b27543a38c1cfeabf3b4e51ec9cdfcd459
Andrew Bituin <fandrewbituin@gmail.com>
2019-01-15 12:48:30 -0800

    added turtle exercises

--- main.js
+++ main.js
@@ -84,3 +84,35 @@ const filteredNames = filter(myNames, function(name) {
 console.log(filteredNames) // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
 
+function hazardWarningCreator(typeOfWarning){
+    let warningCounter = 0;
+    return function(location){
+        warningCounter++;
+        console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+        console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+    
+    }
+}
+
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const hailWarning = hazardWarningCreator('Hail on the Road');
+const sleetWarning = hazardWarningCreator('Sleet on the Road');
+
+rocksWarning('Main St and Pacific Ave');
+rocksWarning('Centinela Ave and Olympic Blvd');
+
+const turtleArray = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+
+const filteredTurtle = turtleArray.filter(function(arr){
+     return arr[0] >= 0 && arr[1] >= 0
+});
+
+const mappedTurtle = turtleArray.map(function(arr){
+    return arr[0] + arr[1];
+});
+
+const loggedTurtle2 = turtleArray.forEach(function(element){
+    console.log(Math.abs(element[0]) + Math.abs(element[1]));
+});
+
+loggedTurtle2;
